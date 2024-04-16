f2be7bad8b37e999de0bd6423149b6bf5e3dfc2e
wanghao@staryea.com <fangrongfan_ext@cop.cmsoft.com.cn>
2024-04-16 10:03:08 +0800

    打包异常修复

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts">
+<script setup>
 import '@/assets/css/screen.css'
-import screen from './views/screen.vue'
+import screen from '@/views/screen.vue'
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,4 @@ import App from './App.vue'
 import router from '@/router/index.js'
 import DataVVue3 from '@kjgl77/datav-vue3'
 
-
 createApp(App).use(router).use(DataVVue3).mount('#app')
